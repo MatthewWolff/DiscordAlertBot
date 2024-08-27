534d8840cee59a0e16942b2ae1b97411cdcce731
Matthew Wolff <mtwol@amazon.com>
2024-08-27 00:52:48 -0700

    canMessageUserAboutGame now has cooloff threshold and works for multiple games simultaneously

--- src/handler/presenceUpdateHandler.ts
+++ src/handler/presenceUpdateHandler.ts
@@ -1,11 +1,10 @@
-import { Presence, time, User } from "discord.js";
+import { Presence, User } from "discord.js";
 import { ActivityType } from 'discord-api-types/v10';
 
 import { DESKTOP, STATUS_ONLINE } from "../config/constants";
 import userAlerts from '../config/gameMap.json'
 import { BaseHandler } from "./baseHandler";
-import { formatDate, getIntersection, getLogger } from "../util";
-import { MessageCore } from "../model";
+import { filter, formatDate, getIntersection, getLogger } from "../util";
 
 const logger = getLogger("handler.PresenceUpdateHandler")
 
@@ -18,8 +17,6 @@ export class PresenceUpdateHandler extends BaseHandler {
             return;
         }
         const user: User = await this.getUser(presence.userId);
-        console.log(user)
-        console.log(presence.activities);
         const activities = presence.activities
             .filter(activity => activity.type === ActivityType.Playing)
             .map(activity => activity.name);
@@ -35,8 +32,7 @@ export class PresenceUpdateHandler extends BaseHandler {
             for (const friendId in userAlerts[user.id]) {
                 const friend: User = await this.getUser(friendId);
                 const subscribedGames: string[] = userAlerts[user.id][friendId];
-                getIntersection(subscribedGames, currentActivities)
-                    .filter(game => this.canMessageUserAboutGame(friend, game))
+                (await filter(getIntersection(subscribedGames, currentActivities), (game: string) => this.canMessageUserAboutGame(friend, game)))
                     .map(game => {
                         logger.info(`User ${user.username} is playing ${game}`);
                         friend.send(`Hey, ${user.globalName} is playing ${game}!`)
@@ -47,45 +43,28 @@ export class PresenceUpdateHandler extends BaseHandler {
         }
     }
 
+    /**
+     * Only message users if they haven't been alerted about a particular game in the past time threshold
+     */
     private async canMessageUserAboutGame(user: User, game: string) {
-        const recentlyMessageUser = await user.createDM()
-            .then(dmChannel => {
-                if (dmChannel.lastMessage){
-                    const timestamp = dmChannel.lastMessage.createdTimestamp
-                    const recent = dmChannel.lastMessage.createdTimestamp > Date.now() - GAME_ALERT_TIME_THRESHOLD_MILLISECONDS;
-                    logger.info(`Last message to ${user.username}: ${dmChannel.lastMessage.content} [${formatDate(timestamp)}]`);
-                    logger.info(`Message is recent: ${recent}`);
-                    return dmChannel.lastMessage.content.includes(game) && recent;
+        return user.createDM().then(dmChannel => dmChannel.messages.fetch({ limit: 100 })
+            .then(messages => {
+                const recentAlertAboutGame = messages
+                    .filter(message => message.author.id === this.client.user.id)
+                    .filter(message => message.content.includes(game))
+                    .filter(message => message.createdTimestamp > Date.now() - GAME_ALERT_TIME_THRESHOLD_MILLISECONDS)
+                    .sort((m1, m2) => m2.createdTimestamp - m1.createdTimestamp)
+                    .first();
+                if (recentAlertAboutGame) {
+                    logger.info(`Already messaged user ${user.username} about ${game} at ${formatDate(recentAlertAboutGame.createdTimestamp)}`)
+                    return false;
+                } else {
+                    return true;
                 }
-                return false;
-            })
-            .catch(e => {
-                logger.error(`Error fetching last message for user ${user.username}`, e);
-                return false;
-            });
-        if (recentlyMessageUser) {
-            logger.info(`Already messaged user ${user.username} about ${game}`);
-            return false;
-        }
-        return true;
 
-        // maybe make this work eventually?
-        // user.dmChannel.messages.fetch({ limit: 100 }).then(messages => {
-        //     const recentAlertAboutGame = messages
-        //         .filter(message => message.author.id === this.client.user.id)
-        //         .filter(message => message.content.includes(game))
-        //         .filter(message => message.createdTimestamp > Date.now() - GAME_ALERT_TIME_THRESHOLD_MILLISECONDS)
-        //         .sort((m1, m2) => m2.createdTimestamp - m1.createdTimestamp)
-        //         .first();
-        //     if (recentAlertAboutGame) {
-        //         logger.info(`Already messaged user ${user.username} about ${game} at ${formatDate(recentAlertAboutGame.createdTimestamp)}`)
-        //         return false;
-        //     } else {
-        //         return true;
-        //     }
-        //
-        // }).catch(e => {
-        //     logger.error(`Error fetching messages for user ${user.username}`, e);
-        // });
+            }).catch(e => {
+                logger.error(`Error fetching messages for user ${user.username}`, e);
+            })
+        )
     }
 }
--- src/util.ts
+++ src/util.ts
@@ -44,3 +44,8 @@ export function formatDate(timestamp: number): string {
     const date = new Date(timestamp);
     return date.toISOString();
 }
+
+export async function filter(arr, callback) {
+    const fail = Symbol()
+    return (await Promise.all(arr.map(async item => (await callback(item)) ? item : fail))).filter(i => i !== fail)
+}
